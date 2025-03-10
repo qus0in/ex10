78530419c6c292634bc3bd3ed5003d99e4d96dc5
qus0in <qus0in@gmail.com>
2025-03-10 11:50:38 +0900

    풋시풋시

--- src/main/java/org/example/ex10/HelloServlet.java
+++ src/main/java/org/example/ex10/HelloServlet.java
@@ -1,15 +1,18 @@
 package org.example.ex10;
 
 import java.io.*;
+
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+// 이름(name)과 경로(value, path)를 지정
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
+    private String nickname;
     public void init() {
-        message = "Hello World!";
+        Dotenv dotenv = Dotenv.load();
+        nickname = dotenv.get("NICKNAME");
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -18,7 +21,7 @@ public class HelloServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + nickname + "</h1>");
         out.println("</body></html>");
     }
 
